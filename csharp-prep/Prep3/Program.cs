bf791a6a5cbd380588efa89bc35eed87889e471d
Hernan Rodriguez <hernan@covr.care>
2024-01-11 00:27:51 +0100

    prep 3 excersise complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+        string userInputGuess;
+        int guess;
+        int attempts  = 0;
+        
+        do
+        {
+            Console.Write("What is your guess? ");
+            userInputGuess = Console.ReadLine();
+            guess = int.Parse(userInputGuess);
+            attempts = attempts + 1;
+
+            if (magicNumber > guess)
+            {
+                Console.WriteLine("Higher");
+            } else if (magicNumber < guess)
+            {
+                Console.WriteLine("Lower");
+            } else 
+            {
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"Answer found in {attempts} attempts");
+            }
+
+        } while (magicNumber != guess);
+        
     }
 }
