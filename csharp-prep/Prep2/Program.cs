e85f5a56f7c9b69370991f27bfa195101b2c870f
Hernan Rodriguez <hernan@covr.care>
2024-01-10 16:21:11 +0100

    prep 2 excersise complete

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter a grade percentage: ");
+        string grade = Console.ReadLine();
+        int numberGrade = int.Parse(grade);
+        char letter;
+        bool pass = true;
+        char sign = ' ';
+
+        if (numberGrade >= 90)
+        {
+            letter = 'A';
+        }
+        else if (numberGrade >= 80)
+        {
+            letter = 'B';
+        }
+        else if (numberGrade >= 70)
+        {
+            letter = 'C';
+        }
+        else if (numberGrade >= 60)
+        {
+            letter = 'D';
+            pass = false;
+        }
+        else
+        {
+            letter = 'F';
+            pass = false;
+        }
+
+        
+        if (numberGrade % 10 >= 7)
+        {
+            sign = '+';
+        } else if (numberGrade % 10 < 3) 
+        {
+            sign = '-';
+        }
+
+        if (numberGrade < 60 || numberGrade >= 97)
+        {
+            sign = ' ';
+        }
+
+        string message = "You failed this class. Better luck next time.";
+        if (pass) 
+        {
+            message = "You passed this class. Congratulations!";
+        }
+        Console.WriteLine($"Your letter grade is: {letter}{(sign != ' ' ? sign.ToString() : "")}. {message}");
+
     }
 }
