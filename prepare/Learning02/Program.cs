4d7033224d1f7d2adb8f1c42f337da1b761b834c
Hernan Rodriguez <hernan@covr.care>
2024-01-16 13:01:47 +0100

    learning week 2 prepr, classes

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+
+        Resume resume = new Resume();
+        Job job1 = new Job();
+        Job job2 = new Job();
+        
+        resume._name = "Allison Rose";
+
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";    
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";    
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        resume.Display();
     }
 }
