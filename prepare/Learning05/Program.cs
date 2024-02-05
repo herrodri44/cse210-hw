d1c749ed0e60d0b5d6458b189f8f52f9c9ba98b0
Hernan Rodriguez <hernan@covr.care>
2024-02-05 11:04:29 +0100

    polimorphism, shape activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square mySquare = new Square("red", 4);
+        Rectangle myRectangle = new Rectangle("red", 8, 3);
+        Circle myCircle = new Circle("red", 2);
+
+        List<Shape> shapeList = new List<Shape>();
+        shapeList.Add(mySquare);
+        shapeList.Add(myRectangle);
+        shapeList.Add(myCircle);
+        
+        foreach (Shape s in shapeList)
+        {
+            double area = s.GetArea();
+            Console.WriteLine($"Shape area: {area}");
+        }
     }
 }
